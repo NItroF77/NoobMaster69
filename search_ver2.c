8786e4567a0cc13526909edf9d33fb312f124a98
NItroF77 <raihanfs77@gmail.com>
2021-11-30 14:20:01 +0700

    tes upload

---
+++ search_ver2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main () {
+   FILE *fp;
+   char str[]="THIS";
+
+   /* opening file for reading */
+   fp = fopen("words.txt" , "r");
+   if(fp == NULL) {
+      perror("Error opening file");
+      return(-1);
+   }
+   while(!feof(fp))
+   {
+      if( fgets (str, 60, fp)!=NULL ) {
+      /* writing content to stdout */
+      puts(str);
+    }
+   }
+   fclose(fp);
+   return(0);
+}
